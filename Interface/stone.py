76ef92fcfc82575bbcd4e838aac5e6b465721fc4
MaltaSailor <plumflood@outlook.com>
2023-09-11 19:31:00 -0400

    Update stone.py

--- Interface/stone.py
+++ Interface/stone.py
@@ -1,3 +1,5 @@
-class Go_Stone:
-    color
-    pos
+class Stone:
+    def __init__(self, color:Color, x:int, y:int)->None:
+        """white->1, void->0, black->-1"""
+        self.color = color
+        self.position = [x, y]
